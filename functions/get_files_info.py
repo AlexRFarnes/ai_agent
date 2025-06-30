65b7676bfc691b9e7cb23ba89883029cb6230847
AlexRFarnes <alexrfarnes@gmail.com>
2025-06-30 13:00:05 +0800

    get files information

---
+++ functions/get_files_info.py
@@ -0,0 +1,32 @@
+from pathlib import Path
+
+
+def get_files_info(working_directory, directory=None):
+    working_path = Path(working_directory).resolve()
+
+    # Check if directory is none
+    if directory is None:
+        return f"Error: {directory} is not a directory"
+
+    # Build the target directory path
+    directory_path = (working_path / directory).resolve()
+
+    # Check if directory is not a directory
+    if not directory_path.is_dir():
+        return f"Error: {directory} is not a directory"
+
+    # Check if directory is outside of the working directory
+    if not directory_path.is_relative_to(working_path):
+        return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+
+    try:
+        files_info = []
+        for filename in directory_path.iterdir():
+            name = filename.name
+            size = filename.stat().st_size
+            is_dir = filename.is_dir()
+            files_info.append(f"- {name}: file_size={size} bytes, is_dir={is_dir}")
+
+        return "\n".join(files_info)
+    except Exception as e:
+        return f"Error listing files: {e}"
---
+++ tests.py
@@ -0,0 +1,35 @@
+import unittest
+
+from functions.get_files_info import get_files_info
+
+
+class TestCalculator(unittest.TestCase):
+    def test_cwd(self):
+        result = get_files_info("calculator", ".")
+        self.assertIn("pkg", result)
+        print(result)
+
+    def test_nested_directory(self):
+        result = get_files_info("calculator", "pkg")
+        self.assertIn("calculator.py", result)
+        print(result)
+
+    def test_of_cwd_1(self):
+        result = get_files_info("calculator", "/bin")
+        self.assertEqual(
+            result,
+            'Error: Cannot list "/bin" as it is outside the permitted working directory',
+        )
+        print(result)
+
+    def test_of_cwd_2(self):
+        result = get_files_info("calculator", "../")
+        self.assertEqual(
+            result,
+            'Error: Cannot list "../" as it is outside the permitted working directory',
+        )
+        print(result)
+
+
+if __name__ == "__main__":
+    unittest.main()
